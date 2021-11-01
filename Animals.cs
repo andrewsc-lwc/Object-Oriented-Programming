e2050079aa02da0e19733eb690546a8adc0a0293
andrewsc <andrewsc@STN-MEDIA-01>
2021-11-01 15:04:58 +0000

    Add project files.

---
+++ Animals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Animals
+{
+	private int size;
+	private string colour;
+	private int age;
+	private int legs;
+
+
+    public Animals(int size, string colour, int age, int legs)
+    {
+        Size = size;
+        Colour = colour;
+        Age = age;
+        Legs = legs;
+    }
+
+    public int Size { get => size; set => size = value; }
+    public string Colour { get => colour; set => colour = value; }
+    public int Age { get => age; set => age = value; }
+    public int Legs { get => legs; set => legs = value; }
+}
+
+public class Cat : Animals
+{
+    private string breed;
+    private string owner;
+
+    public Cat(int size, string colour, int age, int legs, string breed, string owner) : base(size, colour, age, legs)
+    {
+        Breed = breed;
+        Owner = owner;
+    }
+
+    public string Breed { get => breed; set => breed = value; }
+    public string Owner { get => owner; set => owner = value; }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Object_Oriented_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals cat = new (10, "Blue", 21, 5);
+            Console.WriteLine(cat.Size);
+            Cat newCat = new Cat(24, "Green", 3, 4, "Tabby", "Mr Andrews");
+            Console.WriteLine(newCat.Owner);
+        }
+    }
+}
